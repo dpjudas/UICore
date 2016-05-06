5d5f000f203c7ca7eea2d326609e045b229c5eb1
Magnus Norddahl <dpjudas@users.noreply.github.com>
2016-05-06 11:46:27 +0200

    Added vector versions of angle functions

--- Sources/Include/UICore/Core/Math/angle.h
+++ Sources/Include/UICore/Core/Math/angle.h
@@ -30,6 +30,9 @@
 
 #include <memory>
 #include "pi.h"
+#include "vec2.h"
+#include "vec3.h"
+#include "vec4.h"
 
 namespace uicore
 {
@@ -47,24 +50,42 @@ namespace uicore
 	/// Convert a quantity in radians to degrees
 	inline float degrees(float radians) { return radians * 180.0f * (float)(1.0 / PI); }
 	inline double degrees(double radians) { return radians * 180.0 * (1.0 / PI_D); }
+	template<typename T> Vec2<T> degrees(const Vec2<T> &radians) { return Vec2<T>(degrees(radians.x), degrees(radians.y)); }
+	template<typename T> Vec3<T> degrees(const Vec3<T> &radians) { return Vec3<T>(degrees(radians.x), degrees(radians.y), degrees(radians.z)); }
+	template<typename T> Vec4<T> degrees(const Vec4<T> &radians) { return Vec4<T>(degrees(radians.x), degrees(radians.y), degrees(radians.z), degrees(radians.w)); }
 
 	/// Convert a quantity in degrees to radians
 	inline float radians(float degrees) { return degrees * PI * (float)(1.0 / 180.0); }
 	inline double radians(double degrees) { return degrees * PI_D * (1.0 / 180.0); }
+	template<typename T> Vec2<T> radians(const Vec2<T> &degrees) { return Vec2<T>(radians(degrees.x), radians(degrees.y)); }
+	template<typename T> Vec3<T> radians(const Vec3<T> &degrees) { return Vec3<T>(radians(degrees.x), radians(degrees.y), radians(degrees.z)); }
+	template<typename T> Vec4<T> radians(const Vec4<T> &degrees) { return Vec4<T>(radians(degrees.x), radians(degrees.y), radians(degrees.z), radians(degrees.w)); }
 
 	/// \brief Converts angle to range [0,360] degrees.
 	float normalize_360(float degrees);
 	double normalize_360(double degrees);
+	template<typename T> Vec2<T> normalize_360(const Vec2<T> &degrees) { return Vec2<T>(normalize_360(degrees.x), normalize_360(degrees.y)); }
+	template<typename T> Vec3<T> normalize_360(const Vec3<T> &degrees) { return Vec3<T>(normalize_360(degrees.x), normalize_360(degrees.y), normalize_360(degrees.z)); }
+	template<typename T> Vec4<T> normalize_360(const Vec4<T> &degrees) { return Vec4<T>(normalize_360(degrees.x), normalize_360(degrees.y), normalize_360(degrees.z), normalize_360(degrees.w)); }
 
 	/// \brief Converts angle to range [-180,180] degrees.
 	float normalize_180(float degrees);
 	double normalize_180(double degrees);
+	template<typename T> Vec2<T> normalize_180(const Vec2<T> &degrees) { return Vec2<T>(normalize_180(degrees.x), normalize_180(degrees.y)); }
+	template<typename T> Vec3<T> normalize_180(const Vec3<T> &degrees) { return Vec3<T>(normalize_180(degrees.x), normalize_180(degrees.y), normalize_180(degrees.z)); }
+	template<typename T> Vec4<T> normalize_180(const Vec4<T> &degrees) { return Vec4<T>(normalize_180(degrees.x), normalize_180(degrees.y), normalize_180(degrees.z), normalize_180(degrees.w)); }
 
-	/// \brief Converts angle to range [0,2*PI] degrees.
+	/// \brief Converts angle to range [0,2*PI] radians.
 	float normalize_2pi(float radians);
 	double normalize_2pi(double radians);
+	template<typename T> Vec2<T> normalize_2pi(const Vec2<T> &radians) { return Vec2<T>(normalize_2pi(radians.x), normalize_2pi(radians.y)); }
+	template<typename T> Vec3<T> normalize_2pi(const Vec3<T> &radians) { return Vec3<T>(normalize_2pi(radians.x), normalize_2pi(radians.y), normalize_2pi(radians.z)); }
+	template<typename T> Vec4<T> normalize_2pi(const Vec4<T> &radians) { return Vec4<T>(normalize_2pi(radians.x), normalize_2pi(radians.y), normalize_2pi(radians.z), normalize_2pi(radians.w)); }
 
-	/// \brief Converts angle to range [-PI,PI] degrees.
+	/// \brief Converts angle to range [-PI,PI] radians.
 	float normalize_pi(float radians);
 	double normalize_pi(double radians);
+	template<typename T> Vec2<T> normalize_pi(const Vec2<T> &radians) { return Vec2<T>(normalize_pi(radians.x), normalize_pi(radians.y)); }
+	template<typename T> Vec3<T> normalize_pi(const Vec3<T> &radians) { return Vec3<T>(normalize_pi(radians.x), normalize_pi(radians.y), normalize_pi(radians.z)); }
+	template<typename T> Vec4<T> normalize_pi(const Vec4<T> &radians) { return Vec4<T>(normalize_pi(radians.x), normalize_pi(radians.y), normalize_pi(radians.z), normalize_pi(radians.w)); }
 }
