ca83f6cb4691f3ad455bc74e254b1cca9f80ba75
Magnus Norddahl <dpjudas@users.noreply.github.com>
2016-05-16 01:16:00 +0200

    Add doxygen base files

---
+++ Documentation/Overview/main-page.h
@@ -0,0 +1,13 @@
+
+/*! \mainpage UICore Manual
+
+<h2>Reference</h2>
+<ul>
+	<li>\ref Core "Core"</li>
+	<li>\ref Display "Display"</li>
+	<li>\ref Sound "Sound"</li>
+	<li>\ref Network "Network"</li>
+	<li>\ref UI "UI"</li>
+</ul>
+
+*/
